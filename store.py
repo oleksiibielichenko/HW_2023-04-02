7c5ea28a0355214a7876a23bfd36ba420faa00a5
Oleksii Bielichenko <byelichenko@gmail.com>
2023-04-04 23:01:54 +0300

    task

--- store.py
+++ store.py
@@ -1,7 +1,25 @@
+
 # 1) Greeting
 # 2) Context-menu
 #   a) Register new user
 #   q) Quit
 # 3) Show products for user
 # 4) User might buy something and that something must put into Cart
-# 5)
+# 5) After every deal: "Do you want to buy something else?"
+# 6) If No => Get user money and put products from Cart to Bag. If money < something => "Not enough money"
+# 7) Do you want to Exit?
+#   a) Yes => Goodbye
+#   b) No => Return to 1
+#
+#
+# class Store: products = []
+#
+# class User: bag, money => inherit User => SayHello() => print("Hello from User")
+#
+# class Admin: inherit User => SayHello() => print("Hello from Admin")
+#
+# class Product: price, label
+#
+# class Cart: sum_products = [] => getFullPrice(*args, **kwargs)
+#
+# class Bag: bag = []
