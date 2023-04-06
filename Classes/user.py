68396fb8b965d200df55fcc825cef7e3cb75182c
Oleksii Bielichenko <byelichenko@gmail.com>
2023-04-06 21:41:19 +0300

    qqq

---
+++ Classes/user.py
@@ -0,0 +1,6 @@
+class User:
+    def __init__(self, name, password, age, card):
+        self._name = name
+        self._password = password
+        self._age = age
+        self._card = card
--- classes.py
+++ classes.py
@@ -1,19 +1,3 @@
-# class Transport :
-#     wheels = 4
-#
-#     def __init__(self, label, color, price, wheels=4):
-#         self.label = label
-#         self.color = color
-#         self.price = price
-#         self.wheels = wheels
-#
-#
-# class Car(Transport) :
-#     def __init__(self , label , color , price , wheels , drive):
-#         super().__init__(label, color, price, wheels)
-#         self.drive = drive
-
-
 class Store:
     def __init__(self, name, ):
         self.name = name
@@ -37,20 +21,6 @@ class Item:
 class Cart:
     def __init__(self, item):
         self._item = item
-
-
-class User:
-    def __init__(self, name, password, age, card):
-        self._name = name
-        self._password = password
-        self._age = age
-        self._card = card
-
-
-class Admin (User):
-    def __init__(self, name, password, age, card, isAdmin):
-        super().__init__(name, password, age, card)
-        self.isAdmin = isAdmin
 
 
 class Bag:
